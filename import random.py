05d86b0431d30f3af59170e6ff504e2dd7475089
Andy Chin <acbaltic0630@gmail.com>
2021-05-11 23:14:58 +0800

    guess_number

---
+++ import random.py
@@ -0,0 +1,25 @@
+# python在安裝時預設的常用 標準函式庫 standard library--不用安裝，要使用時直接載入使用
+# random --> 一個python檔案即為一個模組 module，很多模組封裝在一起稱為package/套件，放在一個資料夾內
+import random
+
+# 隨機產生包含1及100在內的一個整數
+'''
+r = random.randint(1, 100)
+print(r)
+'''
+
+# 猜數字遊戲
+r = random.randint(1, 100)
+while True:
+    num = input('請猜數字： ')
+    num = int(num)
+    if num == r:
+        print('你猜中了!')
+        break
+    elif num > r:
+        print('比答案大，請再猜')
+    elif num < r:
+        print('比簽案小，請再猜')
+
+
+
